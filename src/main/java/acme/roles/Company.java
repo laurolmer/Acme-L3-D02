7328856ec78c227241a0a88cb86feae040609090
ivasansan1 <ivasansan1@alum.us.es>
2023-02-22 20:09:51 +0100

    Add-- Task 004 ivasansan1

---
+++ src/main/java/acme/roles/Company.java
@@ -0,0 +1,44 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Company extends AbstractRole {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Size(max = 76)
+	protected String			name;
+
+	@NotBlank
+	@Size(max = 26)
+	protected Integer			VAT;
+
+	@NotBlank
+	@Size(max = 101)
+	protected Integer			summary;
+
+	@URL
+	protected String			link;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
