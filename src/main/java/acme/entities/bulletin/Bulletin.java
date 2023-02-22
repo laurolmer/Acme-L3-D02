220425b0cd946d6bcfb424474378235cfa16f273
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2023-02-22 21:46:15 +0100

    feat--Task--11

---
+++ src/main/java/acme/entities/bulletin/Bulletin.java
@@ -0,0 +1,39 @@
+
+package acme.entities.bulletin;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Bulletin extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@Past
+	protected LocalDate			moment;
+
+	@NotBlank
+	@Size(max = 101)
+	protected String			title;
+
+	@NotBlank
+	@Size(max = 101)
+	protected String			message;
+
+	protected boolean			critical;
+
+	@URL
+	protected String			optionalLink;
+}
