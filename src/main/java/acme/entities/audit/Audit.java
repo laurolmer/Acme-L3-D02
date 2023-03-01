00c63d264d71ea777dd805710c1b15e8ffa1680c
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2023-03-01 13:14:49 +0100

    feat--Task--005--Creation of Audit entity

---
+++ src/main/java/acme/entities/audit/Audit.java
@@ -0,0 +1,39 @@
+
+package acme.entities.audit;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Audit extends AbstractEntity {
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			conclusion;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			strong_points;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			weak_points;
+
+}
