bbb23bdda286564e3c594a3fc7984f948951b036
laurolmer <laurolmer@alum.us.es>
2023-03-01 11:57:25 +0100

    Fixed-- Task 015

--- src/main/java/acme/form/AdministratorsDashboard.java
+++ src/main/java/acme/form/AdministratorsDashboard.java
@@ -10,8 +10,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AdministratorsDashboard extends AbstractForm {
+	// Serialisation identifier -----------------------------------------------
 
-	// Total number of principals with each role
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Total number of principals with each role.
 	private Integer				numOfAssistants;
 	private Integer				numOfAuditors;
 	private Integer				numOfCompanies;
@@ -19,20 +24,20 @@ public class AdministratorsDashboard extends AbstractForm {
 	private Integer				numOfProviders;
 	private Integer				numOfLecturers;
 
-	// Ratio of peeps with both an email address and a link
+	// Ratio of peeps with both an email address and a link.
 	private double				linkAndEmailPeepsRatio;
 
-	// Ratios of critical and non-critical bulletins
+	// Ratios of critical and non-critical bulletins.
 	private double				criticalBulletinsRatio;
 	private double				nonCriticalBulletinsRatio;
 
-	// Average, minimum, maximum, and standard deviation of the budget in the offers grouped by currency
+	// Average, minimum, maximum, and standard deviation of the budget in the offers grouped by currency.
 	private Map<String, Double>	avgBudgetByCurrency;
 	private Map<String, Double>	minBudgetByCurrency;
 	private Map<String, Double>	maxBudgetByCurrency;
 	private Map<String, Double>	devBudgetByCurrency;
 
-	// Average, minimum, maximum, and standard deviation of the number of notes posted over the last 10 weeks
+	// Average, minimum, maximum, and standard deviation of the number of notes posted over the last 10 weeks.
 	private double				avgNotesInLast10Weeks;
 	private double				minNotesInLast10Weeks;
 	private double				maxNotesInLast10Weeks;
