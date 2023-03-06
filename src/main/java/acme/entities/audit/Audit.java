5f5635d440972b1a041aa160bcfbd7f7c7499b46
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2023-03-06 11:22:14 +0100

    update--Task--005

--- src/main/java/acme/entities/audit/Audit.java
+++ src/main/java/acme/entities/audit/Audit.java
@@ -1,13 +1,17 @@
 
 package acme.entities.audit;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.entities.audit_records.Audit_Records;
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,23 +21,28 @@ import lombok.Setter;
 @Setter
 public class Audit extends AbstractEntity {
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long		serialVersionUID	= 1L;
 
 	@NotBlank
 	@Column(unique = true)
 	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
-	protected String			code;
+	protected String				code;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			conclusion;
+	protected String				conclusion;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			strong_points;
+	protected String				strong_points;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String			weak_points;
+	protected String				weak_points;
+
+	//Relaciones
+
+	@OneToMany
+	protected List<Audit_Records>	audit_records;
 
 }
