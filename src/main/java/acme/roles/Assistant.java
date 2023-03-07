ecd08eefe348e5ec94110ce51841634607e5f06e
laurolmer <laurolmer@alum.us.es>
2023-03-07 22:17:56 +0100

    Added- Task 008 laurolmer

--- src/main/java/acme/roles/Assistant.java
+++ src/main/java/acme/roles/Assistant.java
@@ -1,18 +1,12 @@
 
 package acme.roles;
 
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
-import acme.entities.tutorials.Tutorial;
 import acme.framework.data.AbstractRole;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,30 +17,26 @@ import lombok.Setter;
 public class Assistant extends AbstractRole {
 
 	// Serialisation identifier -----------------------------------------------
-	protected static final long		serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 	@NotBlank
 	@Length(max = 75)
-	protected String				supervisor;
+	protected String			supervisor;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				resume;
+	protected String			resume;
 
 	@NotBlank
 	@Length(max = 100)
-	protected String				expertiseFields;
+	protected String			expertiseFields;
 
 	@URL
-	protected String				link;
+	protected String			link;
 
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
-	@NotNull
-	@Valid
-	@OneToMany(mappedBy = "assistant")
-	protected Collection<Tutorial>	tutorials;
 
 }
