d3cc4c37f82f4c7052a44634138bc3ed3a8a658e
laurolmer <laurolmer@alum.us.es>
2023-03-08 13:24:24 +0100

    Updated-- Task 010 laurolmer

---
+++ src/main/java/acme/entities/peep/Peep.java
@@ -0,0 +1,53 @@
+
+package acme.entities.peep;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Peep extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@NotNull
+	@PastOrPresent
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				moment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			nick;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			message;
+
+	@URL
+	protected String			link;
+
+	@Email
+	protected String			email;
+
+}
