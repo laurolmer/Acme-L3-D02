2372f9e2378f3524bb6fa65e4f8575d674cdd3f7
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2023-03-08 21:12:26 +0100

    fis-Task-005-Pattern fixed

--- src/main/java/acme/entities/audit/Audit.java
+++ src/main/java/acme/entities/audit/Audit.java
@@ -25,7 +25,7 @@ public class Audit extends AbstractEntity {
 
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
 	protected String			code;
 
 	@NotBlank
