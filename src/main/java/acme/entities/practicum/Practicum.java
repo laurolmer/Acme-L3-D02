497554fc91c165f33ac4dd52a56f1442aa0e9fa2
Yuuta094 <YvanSan094@gmail.com>
2023-03-09 09:13:15 +0100

    Add-- Task 008

--- src/main/java/acme/entities/practicum/Practicum.java
+++ src/main/java/acme/entities/practicum/Practicum.java
@@ -31,7 +31,7 @@ public class Practicum extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@NotBlank
 	@Column(unique = true)
-	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
 	protected String			code;
 
 	@NotBlank
