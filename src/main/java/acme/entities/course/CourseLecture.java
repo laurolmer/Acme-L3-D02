3c15d9533763664dfc5884a00a1e928e0bd6d45a
Albertoperleo <albperleo@alum.us.es>
2023-03-09 17:58:38 +0100

    fix(student1):arreglo de errores comentados en clase
    Refs #109

---
+++ src/main/java/acme/entities/course/CourseLecture.java
@@ -0,0 +1,25 @@
+
+package acme.entities.course;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class CourseLecture extends AbstractEntity {
+
+	//	Serialisation identifier ----------------------------
+	protected static final long serialVersionUID = 1L;
+
+	//	Relationships ---------------------------------------
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Course course;
+
+}
