fe05b3e3ed808b326e0a43140f2582e82aafc56f
Albertoperleo <albperleo@alum.us.es>
2023-03-14 19:49:25 +0100

    feat(sample-data):Implementada sample data individual
    Sample data de course, lecture, lecturer y courseLecture
    
    Refs #23

--- src/main/java/acme/entities/course/Course.java
+++ src/main/java/acme/entities/course/Course.java
@@ -29,7 +29,7 @@ public class Course extends AbstractEntity {
 	//	Attributes -----------------------------------------
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
 	protected String code;
 
 	@NotBlank
